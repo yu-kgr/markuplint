5497518df49913df79f0bf1745aa7f3e2f1ac0f9
Yusuke Hirao <yusukehirao@me.com>
2019-03-26 23:45:22 +0900

    WIP

--- packages/@markuplint/html-ls/gen/index.ts
+++ packages/@markuplint/html-ls/gen/index.ts
@@ -5,6 +5,7 @@ import fetch, { getReferences } from './fetch';
 import { ElementSpec, MLMLSpecJSON } from './types';
 import { getGlobalAttrs } from './global-attrs';
 import { getHTMLElements } from './html-elements';
+import { getAria } from './aria';
 
 const writeFile = util.promisify(fs.writeFile);
 
@@ -13,13 +14,17 @@ async function main() {
 
 	const specs = await getHTMLElements();
 	const globalAttrs = await getGlobalAttrs();
+	const { roles, arias } = await getAria();
 
 	const cites = getReferences();
 
 	const json: MLMLSpecJSON = {
+		$schema: './ml-specs.schema.json',
 		cites,
 		def: {
 			'#globalAttrs': globalAttrs,
+			'#roles': roles,
+			'#ariaAttrs': arias,
 		},
 		specs,
 	};
--- packages/@markuplint/html-ls/gen/types.ts
+++ packages/@markuplint/html-ls/gen/types.ts
@@ -1,19 +1,61 @@
+/**
+ * markuplit Markup-language spec
+ */
 export interface MLMLSpecJSON {
+	$schema?: string;
+
+	/**
+	 * Reference URLs
+	 */
 	cites?: string[];
-	def?: { [name: string]: Attribute[] };
+	def?: {
+		'#globalAttrs'?: Attribute[];
+		'#roles'?: ARIRRoleAttribute[];
+		'#ariaAttrs'?: ARIAAttribute[];
+	};
 	specs: ElementSpec[];
 }
 
+/**
+ * Element spec
+ */
 export type ElementSpec = {
+	/**
+	 * Tag name
+	 */
 	name: string;
+
+	/**
+	 * Reference URL
+	 */
 	cite: string;
+
+	/**
+	 * Description
+	 */
 	description?: string;
+
+	/**
+	 * Element cateogries
+	 */
 	categories: ElementCategories;
+
+	/**
+	 * Permitted content model
+	 */
 	contentModel: {
 		exists: 'required' | 'any';
 		models: string[];
 	}[];
+
+	/**
+	 * Tag omittion
+	 */
 	omittion: ElementSpecOmittion;
+
+	/**
+	 * Attributes
+	 */
 	attributes: (AttributeSpec | string)[];
 };
 
@@ -25,6 +67,8 @@ export type ElementCategories = (
 	  })[];
 
 /**
+ * Element Category
+ *
  * @cite https://html.spec.whatwg.org/multipage/dom.html#kinds-of-content
  */
 export type ElementCategory =
@@ -63,3 +107,32 @@ export type AttributeSpec = Attribute & {
 export type AttributeCtegory = 'global' | 'xml' | 'aria' | 'eventhandler' | 'form' | 'particular';
 
 export type AttributeValue = 'string' | 'space-separated-tokens' | 'function-body' | 'uint' | 'int' | 'float';
+
+export type ARIRRoleAttribute = {
+	name: string;
+	description: string;
+	isAbstract: boolean;
+	generalization: string[];
+	ownedAttribute: string[];
+};
+
+export type ARIAAttribute = {
+	name: string;
+	type: 'property' | 'state';
+	isDeprecated: boolean;
+	value: ARIAAttributeValue;
+	defaultValue?: string;
+};
+
+export type ARIAAttributeValue =
+	| 'true/false'
+	| 'tristate'
+	| 'true/false/undefined'
+	| 'ID reference'
+	| 'ID reference list'
+	| 'integer'
+	| 'number'
+	| 'string'
+	| 'token'
+	| 'token list'
+	| 'URI';
