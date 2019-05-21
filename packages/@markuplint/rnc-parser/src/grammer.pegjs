45ef52809a2ced96324d9ca77ed192cc73497585
Yusuke Hirao <yusukehirao@me.com>
2019-05-21 15:36:44 +0900

    WIP

--- packages/@markuplint/rnc-parser/src/grammer.pegjs
+++ packages/@markuplint/rnc-parser/src/grammer.pegjs
@@ -1,39 +1,44 @@
 start = Doc
 
-Doc = block:(Block+ / _) {
-	return block.filter(_ => _);
+__ = Comment+ / AnySpace
+
+_ = Comment+ / Whitespace
+
+Comment = AnySpace "#" comment:$[^\n]* AnySpace
+
+AnySpace = Whitespace*
+
+Whitespace = [ \t\n\r]+
+
+Doc = block:(Block+ / __ / "") {
+	return block ? block.filter(_ => _) : [];
 }
 
-Block = _ block:(Include /NamespaceDef / DefaultDef / DatatypeDef / Ref / Comment) _ {
+Block = __ block:(Include / NamespaceDef / DatatypeDef / Define) __ {
 	return block;
 }
 
-DatatypeDef = "datatypes" _ def:Ref  {
+DatatypeDef = "datatypes" __ name:($[a-zA-Z0-9._-]) __ "=" __ value:StringToken  {
 	return {
-		...def,
 		type: "datatypes",
+		name,
+		value,
 	}
 }
 
-DefaultDef = "default"  _ def:Ref  {
+NamespaceDef = defaults:("default" __)? "namespace" __ name:($[a-zA-Z0-9._-]+ __)? "=" __ value:StringToken  {
 	return {
-		...def,
-		type: "default",
-	}
-}
-
-NamespaceDef = defaults:("default" / "" ) _ "namespace" _ def:Ref  {
-	return {
-		...def,
-        defaults: !!defaults,
 		type: "namespace",
+		name: name ? name[0] : null,
+		value,
+		isDefault: !!defaults,
 	}
 }
 
-Include = "include" _ file:String _override:(_ "{" Doc "}")? {
+Include = "include" __ filePath:StringToken _override:(__ "{" Doc  "}")? {
 	const res = {
 		type: "include",
-		file,
+		filePath,
 	}
 	if (_override && _override[2]) {
 		res.override = _override[2];
@@ -41,32 +46,67 @@ Include = "include" _ file:String _override:(_ "{" Doc "}")? {
 	return res;
 }
 
-Ref = variableName:Identifier _ defType:("=" / "|=" / "&=") _ value:(Identifiers / String / ListDef / NodeDef / MultipleDef / Identifier) merge:(_ "&" _ Identifier)? {
-	const ref = {
-	type: defType === "=" ? "ref" : defType === "|=" ? "marge-or" : "marge-and",
-		variableName: variableName.name,
-		value,
+Define = name:$[a-zA-Z0-9._-]+ _ defType:("=" / "|=" / "&=") _ contents:(Expression)  {
+	const define = {
+		type: "define",
+		name,
+	};
+	switch (defType) {
+		case "=": define.contents = contents; break;
+		case "|=": define.choice = contents; break;
+		case "&=": define.interleave = contents; break;
 	}
-	if (merge) ref.merge = merge[3]
-	return ref
+	return define;
 }
 
-NodeDef = nodeType:NodeType _ name:(AttrName) _ "{" _  contents:(AttrValue / Identifiers / Identifier) _ "}" required:Required {
-	switch (nodeType) {
-		case "element":
-			return {
-				type: nodeType,
-				...name,
-				contents,
-				required,
-			}
-		case "attribute":
-			return {
-				type: nodeType,
-				...name,
-				value: contents,
-				required,
-		}
+Expression = ex:(Group / Identifiers / Identifier) {
+	return Array.isArray(ex) ? ex : [ex];
+};
+
+Group = "(" __ contents:(Identifiers / Identifier) __ ")" __ required:Required {
+	return {
+		type: "group",
+		contents,
+		...required,
+	}
+}
+
+Identifiers = IdentifiersAndList / IdentifiersOrList / IdentifiersCommaList
+
+IdentifiersOrList = ids:(Identifier __ "|" __)+ lastId:Identifier {
+	return [{choice: [...ids.map(id => id[0]), lastId]}]
+}
+
+IdentifiersAndList = ids:(Identifier __ "&" __)+ lastId:Identifier {
+	return [{interleave: [...ids.map(id => id[0]), lastId]}]
+}
+
+IdentifiersCommaList = ids:(Identifier __ "," __)+ lastId:Identifier {
+	return [...ids.map(id => id[0]), lastId]
+}
+
+Identifier = expression:(Group / Element / Attribute / Data / List / EnumValue / StringValue / Keyword / Ref) required:Required {
+	return {
+		...expression,
+		...required,
+	}
+}
+
+Element = "element" _ name:(ElementName) __ "{" __ contents:Expression? __ "}" {
+	return {
+		type: "element",
+		name,
+		contents,
+	}
+}
+
+ElementName = $[a-zA-Z0-9]+ / "*"
+
+Attribute = "attribute" _ name:(AttrName) __ "{" __  values:(Group / AttrValues) __ "}" {
+	return {
+		type: "attribute",
+		...name,
+		values,
 	}
 }
 
@@ -79,20 +119,6 @@ AttrNameSingle = name:(MultipleNodeName / NodeName) {
 AttrNameWithNot = name:AttrNameSingle not:(_ "-" _ (MultipleNodeName / NodeName)) {
 	return {...name, not: not[3]}
 }
-
-
-ListDef = "list" _ "{" _  contents:(RegExp / AttrValue / Identifiers / Identifier) _ "}" required:Required {
-	return {
-		list: contents,
-		required,
-	}
-}
-
-MultipleDef = "(" _ ids:(Identifiers / Identifier) _ ")" Required  {
-	return ids
-}
-
-NodeType = "element" / "attribute"
 
 MultipleNodeName = "(" _ names:(NodeNamesOrList / NodeName) _ ")"  {
 	return names
@@ -112,120 +138,94 @@ nonnsNodeName = name:($[a-zA-Z0-9-] / "*")+ {
 	return { name: name.join('') }
 }
 
-Identifiers = IdentifiersAndList / IdentifiersOrList / IdentifiersCommaList
+AttrValues = List / EnumValue / AttrRefs
 
-IdentifiersAndList = ids:(Identifier _ "&" _)+ lastId:Identifier {
-	return {and: [...ids.map(id => id[0]), lastId]}
+AttrRefs = refs:(AttrRef __ "|" __)* lastRef:AttrRef {
+	return [...refs.map(ref => ref[0]), lastRef]
 }
 
-IdentifiersOrList = ids:(Identifier _ "|" _)+ lastId:Identifier {
-	return {or: [...ids.map(id => id[0]), lastId]}
-}
+AttrRef = Keyword / Ref
 
-IdentifiersCommaList = ids:(Identifier _ "," _)+ lastId:Identifier {
-	return {comma: [...ids.map(id => id[0]), lastId]}
-}
-
-Identifier = id:(MultipleDef / ListDef / RegExp / TypedValue / Variable / Keyword) required:Required {
-	id.required = required
-	return id
-}
-
-TypedValue = type:ValueType _ rawValue:String {
+Ref = ns:($[a-zA-Z0-9]+ ":")? name:$[a-zA-Z0-9-.]+ {
+	if (ns) {
+		return {
+			type: "ref",
+			ns: ns[0],
+			name,
+		}
+	}
 	return {
-		type,
-		rawValue,
+		type: "ref",
+		name,
 	}
 }
 
 Keyword = keyword:("empty" / "notAllowed" / "text" / "token") {
 	return {
 		type:"keyword",
-		value: keyword
+		name: keyword
 	}
 }
 
-
-Variable = nsVariable / nonnsVariable
-
-nsVariable = ns:$[a-zA-Z0-9]+ ":" name:nonnsVariable {
-	return { ...name, ns }
-}
-
-nonnsVariable = name:$[a-zA-Z0-9-.]+ {
+List = "list" _ "{" __  values:((Data / Keyword / Ref) Required)* __ "}" {
 	return {
-		type: "variable",
-		name,
+		type: "list",
+		values: values.map(item => {
+			return {
+				...item[0],
+				...item[1],
+			}
+		}),
 	}
 }
 
-AttrValue =  values:(Constant _ "|" _)+ lastValue:Constant {
-	return [...values.map(v => v[0]), lastValue]
+EnumValue = values:(StringValue __ "|" __)* lastValue:StringValue {
+	return {
+		type: "enum",
+		ns: lastValue.ns,
+		list: [...values.map(v => v[0].value), lastValue.value]
+	}
 }
 
-Constant = valueType:ValueType _ value:String {
+StringValue = ns:($[a-z0-9]+ ":")? "string" __ value:StringToken {
 	return {
-		type: "constant",
-		valueType,
+		type: "string",
+		ns: ns ? ns[0] : null,
 		value,
 	}
 }
 
-RegExp = RegExpWithMinLength / RegExpPatternOnly
-
-RegExpPatternOnly = valueType:ValueType _ "{" _ "pattern" _ "=" _ pattern:String  _ "}" required:Required {
+Data = ns:($[a-z0-9]+ ":")? type:$[a-z0-9]+ __ "{" __ params:DataParams __ "}" {
 	return {
-		type: "regexp-pattern",
-		pattern,
+		type,
+		ns: ns ? ns[0] : null,
+		params,
 	}
 }
 
-RegExpWithMinLength = valueType:ValueType _ "{" _ "pattern" _ "=" _ pattern:String  _ "minLength" _ "=" _ minLength:String _  "}" required:Required {
+DataParams = firstParam:DataParam params:(_ DataParam)*  {
+	let result = {...firstParam};
+	for (const p of params) {
+		result = {...result, ...p[1]};
+	}
+	return result;
+}
+
+DataParam = prop:$[a-z0-9]+ __ "=" __ value:StringToken {
 	return {
-		type: "regexp-pattern",
-		pattern,
-        minLength,
+		[prop]: value,
 	}
 }
 
-
-ValueType = nsValueType / nonnsValueType
-
-nsValueType = ns:$[a-zA-Z0-9]+ ":" valueType:nonnsValueType {
-	return { ...valueType, ns }
-}
-
-nonnsValueType = valueType:ValueTypeEnum {
-	return { valueType }
-}
-
-ValueTypeEnum = "string" / "token"
-
-String = DoubleQuoteString / SingleQuoteString
-
-DoubleQuoteString = str:('"' $[^"]* '"') {
+StringToken = str:('"' $[^"]* '"') {
 	return str[1]
 }
 
-SingleQuoteString = str:("'" $[^']* "'") {
-	return str[1]
-}
-
-Required = _ token:("*" / "+" / "?" / "") {
+Required = token:("*" / "+" / "?" / "") {
 	switch (token) {
-		case "*": return "zeroOrMore";
-		case "+": return "oneOrMore";
-		case "?": return "optional"
-		case "": return "required"
+		case "*": return {required: "zeroOrMore"};
+		case "+": return {required:"oneOrMore"};
+		case "?": return {required:"optional"};
+		case "": return {};
 	}
-}
-
-_ = Comment+ / WHITESPACE
-
-Comment = WHITESPACE "#" comment:$[^\n]* WHITESPACE {
-	return null
-}
-
-WHITESPACE = [ \t\n\r]* {
-   return null
 }
--- packages/@markuplint/rnc-parser/tests/tokenizer.spec.js
+++ packages/@markuplint/rnc-parser/tests/tokenizer.spec.js
@@ -1,17 +1,536 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { parse } = require('../lib/_parser');
+const { parse } = require('../lib/_tokenizer');
 
-describe('tokenizer', () => {
-	it('is reference grammer', () => {
-		expect(parse('a = a')).toStrictEqual([
-			{
-				type: 'ref',
-				variableName: 'a',
-				value: {
+describe('Datatypes', () => {
+	it('is datatypes', () => {
+		expect(parse('datatypes w = "http://whattf.org/datatype-draft"')[0]).toStrictEqual({
+			type: 'datatypes',
+			name: 'w',
+			value: 'http://whattf.org/datatype-draft',
+		});
+	});
+});
+
+describe('Namespace', () => {
+	it('is default namespace', () => {
+		expect(parse('default namespace = "http://www.w3.org/1999/xhtml"')[0]).toStrictEqual({
+			type: 'namespace',
+			name: null,
+			value: 'http://www.w3.org/1999/xhtml',
+			isDefault: true,
+		});
+	});
+
+	it('is namespace', () => {
+		expect(parse('namespace svg = "http://www.w3.org/2000/svg"')[0]).toStrictEqual({
+			type: 'namespace',
+			name: 'svg',
+			value: 'http://www.w3.org/2000/svg',
+			isDefault: false,
+		});
+	});
+});
+
+describe('Includeing', () => {
+	it('is include', () => {
+		expect(parse('include "foo.rnc"')[0]).toStrictEqual({
+			type: 'include',
+			filePath: 'foo.rnc',
+		});
+	});
+
+	it('is namespace', () => {
+		expect(parse('include "bar.rnc" { a = b }')[0]).toStrictEqual({
+			type: 'include',
+			filePath: 'bar.rnc',
+			override: [
+				{
+					type: 'define',
 					name: 'a',
-					required: 'required',
-					type: 'variable',
+					contents: [
+						{
+							type: 'ref',
+							name: 'b',
+						},
+					],
 				},
+			],
+		});
+	});
+});
+
+describe('Definition', () => {
+	it('is definition', () => {
+		expect(parse('a = a')[0]).toStrictEqual({
+			type: 'define',
+			name: 'a',
+			contents: [
+				{
+					type: 'ref',
+					name: 'a',
+				},
+			],
+		});
+	});
+
+	it('is definition (choice)', () => {
+		expect(parse('a |= a')[0]).toStrictEqual({
+			type: 'define',
+			name: 'a',
+			choice: [
+				{
+					type: 'ref',
+					name: 'a',
+				},
+			],
+		});
+	});
+
+	it('is definition (interleave)', () => {
+		expect(parse('a &= a')[0]).toStrictEqual({
+			type: 'define',
+			name: 'a',
+			interleave: [
+				{
+					type: 'ref',
+					name: 'a',
+				},
+			],
+		});
+	});
+});
+
+describe('Order', () => {
+	it('is choice list', () => {
+		expect(parse('a = a | b')[0]).toStrictEqual({
+			type: 'define',
+			name: 'a',
+			contents: [
+				{
+					choice: [
+						{
+							type: 'ref',
+							name: 'a',
+						},
+						{
+							type: 'ref',
+							name: 'b',
+						},
+					],
+				},
+			],
+		});
+	});
+
+	it('is choice list', () => {
+		expect(parse('a = (a | b)')[0]).toStrictEqual({
+			type: 'define',
+			name: 'a',
+			contents: [
+				{
+					type: 'group',
+					contents: [
+						{
+							choice: [
+								{
+									type: 'ref',
+									name: 'a',
+								},
+								{
+									type: 'ref',
+									name: 'b',
+								},
+							],
+						},
+					],
+				},
+			],
+		});
+	});
+
+	it('is interleave list', () => {
+		expect(parse('a = a & b & c')[0]).toStrictEqual({
+			type: 'define',
+			name: 'a',
+			contents: [
+				{
+					interleave: [
+						{
+							type: 'ref',
+							name: 'a',
+						},
+						{
+							type: 'ref',
+							name: 'b',
+						},
+						{
+							type: 'ref',
+							name: 'c',
+						},
+					],
+				},
+			],
+		});
+	});
+
+	it('is list', () => {
+		expect(parse('a = a , b , c')[0]).toStrictEqual({
+			type: 'define',
+			name: 'a',
+			contents: [
+				{
+					type: 'ref',
+					name: 'a',
+				},
+				{
+					type: 'ref',
+					name: 'b',
+				},
+				{
+					type: 'ref',
+					name: 'c',
+				},
+			],
+		});
+	});
+});
+
+describe('Grouping', () => {
+	it('is deep nesting', () => {
+		expect(parse('a = (b* & (c+ | d)?)')[0]).toStrictEqual({
+			type: 'define',
+			name: 'a',
+			contents: [
+				{
+					type: 'group',
+					contents: [
+						{
+							interleave: [
+								{
+									type: 'ref',
+									name: 'b',
+									required: 'zeroOrMore',
+								},
+								{
+									type: 'group',
+									contents: [
+										{
+											choice: [
+												{
+													type: 'ref',
+													name: 'c',
+													required: 'oneOrMore',
+												},
+												{
+													type: 'ref',
+													name: 'd',
+												},
+											],
+										},
+									],
+									required: 'optional',
+								},
+							],
+						},
+					],
+				},
+			],
+		});
+	});
+
+	it('is deep nesting', () => {
+		expect(parse('a = (b?, c*, d*, c*, e?, c*, ((e* | f+), c*, g?))')[0]).toStrictEqual({
+			type: 'define',
+			name: 'a',
+			contents: [
+				{
+					type: 'group',
+					contents: [
+						{
+							type: 'ref',
+							name: 'b',
+							required: 'optional',
+						},
+						{
+							type: 'ref',
+							name: 'c',
+							required: 'zeroOrMore',
+						},
+						{
+							type: 'ref',
+							name: 'd',
+							required: 'zeroOrMore',
+						},
+						{
+							type: 'ref',
+							name: 'c',
+							required: 'zeroOrMore',
+						},
+						{
+							type: 'ref',
+							name: 'e',
+							required: 'optional',
+						},
+						{
+							type: 'ref',
+							name: 'c',
+							required: 'zeroOrMore',
+						},
+						{
+							type: 'group',
+							contents: [
+								{
+									type: 'group',
+									contents: [
+										{
+											choice: [
+												{
+													type: 'ref',
+													name: 'e',
+													required: 'zeroOrMore',
+												},
+												{
+													type: 'ref',
+													name: 'f',
+													required: 'oneOrMore',
+												},
+											],
+										},
+									],
+								},
+								{
+									type: 'ref',
+									name: 'c',
+									required: 'zeroOrMore',
+								},
+								{
+									type: 'ref',
+									name: 'g',
+									required: 'optional',
+								},
+							],
+						},
+					],
+				},
+			],
+		});
+	});
+});
+
+describe('Element', () => {
+	it('is Element (content empty)', () => {
+		expect(parse('a = element a {}')[0]).toStrictEqual({
+			type: 'define',
+			name: 'a',
+			contents: [
+				{
+					type: 'element',
+					name: 'a',
+					contents: null,
+				},
+			],
+		});
+	});
+
+	it('is Element', () => {
+		expect(parse('a = element a { sub }')[0]).toStrictEqual({
+			type: 'define',
+			name: 'a',
+			contents: [
+				{
+					type: 'element',
+					name: 'a',
+					contents: [
+						{
+							type: 'ref',
+							name: 'sub',
+						},
+					],
+				},
+			],
+		});
+	});
+});
+
+describe('Attribute', () => {
+	it('is Attribute (content ref)', () => {
+		expect(parse('a = attribute attrName { sub }')[0].contents).toStrictEqual([
+			{
+				type: 'attribute',
+				name: 'attrName',
+				values: [
+					{
+						type: 'ref',
+						name: 'sub',
+					},
+				],
+			},
+		]);
+	});
+
+	it('is Attribute (content keyword)', () => {
+		expect(parse('a = attribute attrName { text }')[0].contents).toStrictEqual([
+			{
+				type: 'attribute',
+				name: 'attrName',
+				values: [
+					{
+						type: 'keyword',
+						name: 'text',
+					},
+				],
+			},
+		]);
+	});
+
+	it('is Attribute (content list)', () => {
+		expect(parse('a = attribute attrName { sub | text }')[0].contents).toStrictEqual([
+			{
+				type: 'attribute',
+				name: 'attrName',
+				values: [
+					{
+						type: 'ref',
+						name: 'sub',
+					},
+					{
+						type: 'keyword',
+						name: 'text',
+					},
+				],
+			},
+		]);
+	});
+
+	it('is Attribute (content enum)', () => {
+		expect(parse('a = attribute attrName { string "" | string "foo" }')[0].contents).toStrictEqual([
+			{
+				type: 'attribute',
+				name: 'attrName',
+				values: {
+					type: 'enum',
+					ns: null,
+					list: ['', 'foo'],
+				},
+			},
+		]);
+	});
+
+	it('is Attribute (content enum one string)', () => {
+		expect(parse('a = attribute attrName { string "foo" }')[0].contents).toStrictEqual([
+			{
+				type: 'attribute',
+				name: 'attrName',
+				values: {
+					type: 'enum',
+					ns: null,
+					list: ['foo'],
+				},
+			},
+		]);
+	});
+
+	it('is Attribute (content enum with namespace)', () => {
+		expect(parse('a = attribute attrName { w:string "" | w:string "foo" }')[0].contents).toStrictEqual([
+			{
+				type: 'attribute',
+				name: 'attrName',
+				values: {
+					type: 'enum',
+					ns: 'w',
+					list: ['', 'foo'],
+				},
+			},
+		]);
+	});
+});
+
+describe('Value list', () => {
+	it('is value list', () => {
+		expect(parse('a = list { token* }')[0].contents).toStrictEqual([
+			{
+				type: 'list',
+				values: [
+					{
+						type: 'keyword',
+						name: 'token',
+						required: 'zeroOrMore',
+					},
+				],
+			},
+		]);
+	});
+
+	it('is value list (required)', () => {
+		expect(parse('a = list { token }')[0].contents).toStrictEqual([
+			{
+				type: 'list',
+				values: [
+					{
+						type: 'keyword',
+						name: 'token',
+					},
+				],
+			},
+		]);
+	});
+
+	it('is value ref list', () => {
+		expect(parse('a = list { name:refs+ }')[0].contents).toStrictEqual([
+			{
+				type: 'list',
+				values: [
+					{
+						type: 'ref',
+						ns: 'name',
+						name: 'refs',
+						required: 'oneOrMore',
+					},
+				],
+			},
+		]);
+	});
+});
+
+describe('Data with params', () => {
+	it('is data', () => {
+		expect(parse('a = xsd:string { pattern = "[a-z]+" }')[0].contents).toStrictEqual([
+			{
+				type: 'string',
+				ns: 'xsd',
+				params: {
+					pattern: '[a-z]+',
+				},
+			},
+		]);
+	});
+
+	it('is data (2 params)', () => {
+		expect(parse('a = xsd:string { pattern = "[a-z]+" length="2" }')[0].contents).toStrictEqual([
+			{
+				type: 'string',
+				ns: 'xsd',
+				params: {
+					pattern: '[a-z]+',
+					length: '2',
+				},
+			},
+		]);
+	});
+
+	it('is data in list', () => {
+		expect(parse('a = list { xsd:string { pattern = "[a-z]+" }}')[0].contents).toStrictEqual([
+			{
+				type: 'list',
+				values: [
+					{
+						type: 'string',
+						ns: 'xsd',
+						params: {
+							pattern: '[a-z]+',
+						},
+					},
+				],
 			},
 		]);
 	});
